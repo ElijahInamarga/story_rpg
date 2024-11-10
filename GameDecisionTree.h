9dc91fcdf9c345eaa024a9208235a6158b41cae3
ElijahInamarga <elijahcek@gmail.com>
2024-11-09 20:06:30 -0800

    Completed play game function

--- GameDecisionTree.h
+++ GameDecisionTree.h
@@ -13,34 +13,35 @@ template <typename T>
 class GameDecisionTree {
 private:
     Node<T>* rootPtr;
+    std::vector<Node<T>> tempList;
 public:
     GameDecisionTree() : rootPtr(nullptr) {}
 
-    void treeify(std::vector<Node<T>>* vectorPtr, Node<T>* nodePtr) {
+    void treeify(Node<T>* nodePtr) {
         int leftIndex = nodePtr->data.leftEventNumber - 1;
         int rightIndex = nodePtr->data.rightEventNumber - 1;
 
         // Base case 1
-        if(leftIndex >= vectorPtr->size() || leftIndex < 0) {
+        if(leftIndex >= tempList.size() || leftIndex < 0) {
             return;
         }
 
-        Node<T>* leftChild = &(*vectorPtr)[leftIndex];
+        Node<T>* leftChild = &tempList[leftIndex];
         nodePtr->left = leftChild;
-        treeify(vectorPtr, leftChild);
+        treeify(leftChild);
+
         // Base case 2
-        if(rightIndex >= vectorPtr->size() || rightIndex < 0) {
+        if(rightIndex >= tempList.size() || rightIndex < 0) {
             return;
         }
-        
-        Node<T>* rightChild = &(*vectorPtr)[rightIndex];
+
+        Node<T>* rightChild = &tempList[rightIndex];
         nodePtr->right = rightChild;
-        treeify(vectorPtr, rightChild);
+        treeify(rightChild);
     }
 
     // Function to load story data from a text file and build the binary tree
-    void loadStoryFromFile(const string& filename, char delimiter) {
-        std::vector<Node<T>> tempList;
+    void loadStoryFromFile(const std::string& filename, char delimiter) {
         std::ifstream storyFile(filename);
         if(!storyFile.is_open()) {
             std::cout << "Error: storyFile failed to open" << std::endl;
@@ -68,19 +69,43 @@ public:
         }
         storyFile.close();
 
-        // REMOVE TEST LATER
-        // for(int i = 0; i < tempList.size(); i++) {
-        //     std::cout << tempList[i].data.eventNumber << tempList[i].data.description << tempList[i].data.leftEventNumber << tempList[i].data.rightEventNumber << std::endl;
-        // }
-
         // Convert vector into binary tree
         rootPtr = &(tempList[0]); // Tree starts with vector[0]
-        treeify(&tempList, rootPtr);
+        treeify(rootPtr);
     }
 
     // Function to start the game and traverse the tree based on user input
     void playGame() {
-        std::cout << rootPtr->data.description << std::endl;
+        Node<T>* currentPtr = rootPtr;
+        while(true) {
+            std::string choice;
+            std::cout << currentPtr->data.description << std::endl;
+            if(currentPtr->data.leftEventNumber == -1 && currentPtr->data.rightEventNumber == -1) return;
+    
+            if(currentPtr->data.leftEventNumber != -1 && currentPtr->data.rightEventNumber != -1) {\
+                std::cout << "Do you go left or right?" << std::endl;
+                cin >> choice;
+                if(choice == "left" || choice == "Left") {
+                    currentPtr = currentPtr->left;
+                }
+                if(choice == "right" || choice == "Right") {
+                    currentPtr = currentPtr->right;
+                }
+                continue;
+            }
+
+            if(currentPtr->data.rightEventNumber == -1) {
+                std::cout << "There is only one choice. You go left." << std::endl;
+                currentPtr = currentPtr->left;
+                continue;
+            }
+
+            if(currentPtr->data.leftEventNumber == -1) {
+                std::cout << "There is only one choice. You go right." << std::endl;
+                currentPtr = currentPtr->right;
+                continue;
+            }
+        }
     }
 };
 
